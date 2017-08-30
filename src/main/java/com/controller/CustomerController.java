bb1c4790e1754376a9de0d9896a8f498fb6a003c
Patrick Leee <patrickleee@foxmail.com>
2017-08-30 15:35:51 +0800

    添加thymeleaf模板

--- src/main/java/com/controller/CustomerController.java
+++ src/main/java/com/controller/CustomerController.java
@@ -4,7 +4,9 @@ import com.model.Customer;
 import com.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,16 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
  * CreateTime 2017/7/26 17:00
  * UpdateTime 2017/7/26 17:00
  */
-@RestController
+@Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
     @Autowired
     CustomerService customerService;
 
-    @RequestMapping(value = "/one")
-    public void getOne() {
+    @RequestMapping(value = "/one", method = RequestMethod.GET)
+    public String getOne(Model model) {
         Customer customer = customerService.findCustomerByUserId("1101");
+        model.addAttribute("customer", customer);
         System.out.println(customer.toString());
+
+        return "customer";
     }
 }
